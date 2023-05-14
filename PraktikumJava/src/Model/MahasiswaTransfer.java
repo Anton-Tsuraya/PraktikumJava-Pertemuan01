5697fb5a1b8ebc2c83c80ddefc1db8e1b461a9d3
Anton <129922565+Anton-Tsuraya@users.noreply.github.com>
2023-05-14 13:19:02 +0800

    Revert "Create Mahasiswa.java"
    This reverts commit 0e26b99e974c4f75fad0b9fe1ae771654eab9d3f.

---
+++ PraktikumJava/src/Model/MahasiswaTransfer.java
@@ -0,0 +1,19 @@
+package Model;
+
+public class MahasiswaTransfer extends Mahasiswa {
+    private double IPKSebelumnya;
+    
+    public double getIPKSebelumnya() {
+        return IPKSebelumnya;
+    }
+
+    public void setIPKSebelumnya(double IPKSebelumnya) {
+        this.IPKSebelumnya = IPKSebelumnya;
+    }
+  
+        public void inputKRS () {
+            super.inputKRS();
+            System.out.println("AAA Input KRS Sebelumnya");
+        }
+
+}
