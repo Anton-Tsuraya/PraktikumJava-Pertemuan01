ddc4d8c05faae7a8f491abd4768bdf06b0bb8814
Anton <129922565+Anton-Tsuraya@users.noreply.github.com>
2023-05-14 13:30:09 +0800

    Update Latihan1.java

--- Latihan1/src/Latihan1/Latihan1.java
+++ Latihan1/src/Latihan1/Latihan1.java
@@ -1,22 +1,20 @@
 package Latihan1;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import Model2.Pulpen;
 
-/**
- *
- * @author ANTON TSURAYA
- */
 public class Latihan1 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Pulpen pulpen = new Pulpen ();
+        
+        pulpen.setNama("ANTON TSURAYA");
+        pulpen.setBerat("10 Kg");
+        pulpen.setPanjang(10);
+        pulpen.setHarga(5.000);
+        
+        pulpen.viewData();
+            
     }
     
 }
